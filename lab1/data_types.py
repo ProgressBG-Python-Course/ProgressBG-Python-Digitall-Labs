56354769c9b52c4ba69152a0e8808f1e538133ec
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2024-01-15 17:35:33 +0200

    Lab1: 15.01.2024 DONE: Lecture 2

---
+++ lab1/data_types.py
@@ -0,0 +1,79 @@
+# ------------------------------ Numerical Types ----------------------------- #
+# import math
+
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(0.1+0.2)
+
+# x = 3.923
+# print( math.floor(x) )
+# print(round(x))
+# print( round(x) )
+
+
+# ---------------------------------- Strings --------------------------------- #
+# print( '1' )
+# print( "1" )
+# # print( "1' )
+# print( 1 )
+# text_single_line = 'Single-line strings literal should be closed in single or double quotes'
+
+# text_multi = '''Strings are immutable sequences of Unicode code points (will be discussed further).
+# Single-line strings literal should be closed in single or double quotes
+# No difference between single or double quoted strings!
+# Multi-line strings literal should be closed in triple single or double quotes'''
+
+# print( 1 + 1)
+# # print( '1' + 1)
+# print('~' * 50)
+
+# name = 'ada'
+# print( name.capitalize() )
+# print( name.replace('ad','@') )
+
+# --------------------------------- Variables -------------------------------- #
+# x = 5
+# print( x )
+
+# RAM:
+#     x: 123: 5
+
+
+# user_name = 'Ada'     # snake case => Pythonic
+# userSurname= 'Byron'  # camel case => not Pythonic
+# user_age = 23
+
+# print(user_name)
+# print( userSurname )
+# print( user_age )
+
+# x = 5
+# y = 2+3
+
+# print( id(x) )
+# print( id(y) )
+
+
+# x = 221_983_456
+# y = 221_983_456
+
+# print( id(x) )
+# print( id(y) )
+
+# RAM:
+#     x,y: 121: 5
+#
+
+
+# a = 5
+# print(id(a))
+# #
+
+# # Now we create a new object, and bind it to 'a'
+# a = 6
+# print(id(a))
+# # 9413344
+
+# VSCode :
+# CTRL+/ = > toggle comments
